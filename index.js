73768ceee54157947e47026eb07da1f6aba79bb0
Brice Chevalier <bchevalier@wizcorp.jp>
2014-02-05 12:00:47 +0900

    * events component

---
+++ index.js
@@ -0,0 +1,122 @@
+
+
+var EventEmitter = function () {
+	this.eventHandlers = {};
+};
+EventEmitter.EventEmitter = EventEmitter;
+module.exports = EventEmitter;
+
+EventEmitter.prototype.on = function (evt, fn) {
+	if (typeof fn !== 'function') {
+		console.warn('Tried to register non-function', fn, 'as event handler for event:', evt);
+		return;
+	}
+
+	this.emit('newListener', evt, fn);
+
+	var allHandlers = this.eventHandlers;
+	var evtHandlers = allHandlers[evt];
+	if (evtHandlers === undefined) {
+		// first event handler for this event type
+		allHandlers[evt] = [fn];
+		return;
+	}
+
+	evtHandlers.push(fn);
+};
+
+EventEmitter.prototype.add = EventEmitter.prototype.on;
+
+EventEmitter.prototype.once = function (evt, fn) {
+	fn.once = 1 + (fn.once >>> 0);
+
+	this.on(evt, fn);
+};
+
+
+EventEmitter.prototype.removeListener = function (evt, handler) {
+	// like node.js, we only remove a single listener at a time, even if it occurs multiple times
+
+	var handlers = this.eventHandlers[evt];
+	if (handlers !== undefined) {
+		var index = handlers.indexOf(handler);
+		if (index !== -1) {
+			handlers.splice(index, 1);
+			this.emit('removeListener', evt, handler);
+			if (handlers.length === 0) {
+				delete this.eventHandlers[evt];
+			}
+		}
+	}
+};
+
+
+EventEmitter.prototype.removeAllListeners = function (evt) {
+	if (evt) {
+		delete this.eventHandlers[evt];
+	} else {
+		this.eventHandlers = {};
+	}
+};
+
+
+EventEmitter.prototype.hasListeners = function (evt) {
+	return (this.eventHandlers[evt] !== undefined);
+};
+
+
+EventEmitter.prototype.listeners = function (evt) {
+	var handlers = this.eventHandlers[evt];
+	if (handlers !== undefined) {
+		var len = handlers.length;
+
+		var fns = new Array(len);
+
+		for (var i = 0; i < len; i++) {
+			fns[i] = handlers[i];
+		}
+
+		return fns;
+	}
+
+	return [];
+};
+
+
+EventEmitter.prototype.emit = function (evt) {
+
+	var handlers = this.eventHandlers[evt];
+	if (handlers === undefined) {
+		return;
+	}
+
+	// copy handlers into a new array, so that handler removal doesn't affect array length
+	handlers = handlers.slice();
+
+	var args = Array.apply(null, arguments);
+	args.shift();
+
+	for (var i = 0, len = handlers.length; i < len; i++) {
+		var handler = handlers[i];
+		if (handler === undefined) {
+			continue;
+		}
+
+		var result = handler.apply(this, args);
+
+		if (handler.once) {
+			if (handler.once > 1) {
+				handler.once--;
+			} else {
+				delete handler.once;
+			}
+
+			this.removeListener(evt, handler);
+		}
+
+		if (result === false) {
+			// cancelBubble
+			break;
+		}
+	}
+};
